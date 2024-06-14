c7b4f3266e30b2ec0658d3a597a7d54a0122ea0c
wjseoduq3 <dragon65@nate.com>
2024-06-14 16:21:18 +0900

    1'st commit

---
+++ src/main/java/com/jdy/test/TestController.java
@@ -0,0 +1,13 @@
+package com.jdy.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value = "/hello")
+	public String hello() {
+		return "hello";
+	}
+}
